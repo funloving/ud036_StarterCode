7c046369a7f1c539fd85c35782218b74cc6e2620
funloving <aparajita.85@gmail.com>
2017-10-15 21:20:06 +0530

    Pep 8 changes made

--- entertainment.py
+++ entertainment.py
@@ -1,11 +1,19 @@
 import media
 import fresh_tomatoes
-#Code to create instances of class media and create movies website
-A_walk_to_remember = media.Movie("A Walk To Remember", "https://images-na.ssl-images-amazon.com/images/M/MV5BZDMzNWFjYzUtY2E1ZC00MzhiLThlOWUtOTkxMTI3MGQ0ZTVmL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTE2ODAyODI@._V1_.jpg", "https://www.youtube.com/watch?v=k3B2XBcp7vA")
-The_Mummy = media.Movie("The Mummy", "https://i.ytimg.com/vi/GzorZUuZqEI/maxresdefault.jpg", "https://www.youtube.com/watch?v=IjHgzkQM2Sg")
-Terminator = media.Movie("Terminator", "http://worldversus.com/img/terminator.jpg", "https://www.youtube.com/watch?v=jNU_jrPxs-0")
-Sholay = media.Movie("Sholay", "http://fm.indsyntest.com/wordpress/wp-content/uploads/1970/01/01/sholay_120810.jpg", "https://www.youtube.com/watch?v=hLhzpe3_V_g")
+# Code to create instances of class media and create movies website
+# This is the primary code that calls the other classes. To create the links
+# for other movies, simply fill out the details below
+# for example movie = media.Movie('<Movie Name>',thumbnail link,trailer link)
+A_walk_to_remember = media.Movie("A Walk To Remember", "http://bit.ly/2vFLXV7",
+                                 "https://www.youtube.com/watch?v=k3B2XBcp7vA")
+The_Mummy = media.Movie("The Mummy",
+                        "https://i.ytimg.com/vi/GzorZUuZqEI/maxresdefault.jpg",
+                        "https://www.youtube.com/watch?v=IjHgzkQM2Sg")
+Terminator = media.Movie("Terminator",
+                         "http://worldversus.com/img/terminator.jpg",
+                         "https://www.youtube.com/watch?v=jNU_jrPxs-0")
+Sholay = media.Movie("Sholay", "http://bit.ly/2idIuLE",
+                     "https://www.youtube.com/watch?v=hLhzpe3_V_g")
 movies = [A_walk_to_remember, The_Mummy, Terminator, Sholay]
 fresh_tomatoes.open_movies_page(movies)
-#print (media.Movie.__doc__)
-
+# print (media.Movie.__doc__)
